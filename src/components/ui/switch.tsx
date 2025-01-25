6d14cb5ba6a9bef322e484b5c055fe26d522eee3
Nikolaj Sokolowski <n.sokolowski@neusta.de>
2025-01-25 22:28:05 +0100

    controllabel animation

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -9,7 +9,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-5 w-10 shrink-0 cursor-pointer items-center  border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-3a-white data-[state=unchecked]:bg-3a-gray",
+      "peer relative flex h-px w-6 shrink-0 cursor-pointer items-center bg-3a-gray transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-3a-white",
       className
     )}
     {...props}
@@ -17,7 +17,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-4 w-4  bg-3a-gray-darkest shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 "
+        "block h-5 w-px bg-3a-white transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0  relative after:content-[''] after:absolute after:top-0 after:bottom-0 after:left-1/2 after:w-px after:-translate-x-1/2 after:bg-3a-white before:content-[''] before:absolute before:bottom-0 before:left-[-1rem] before:top-1/2 before:-translate-y-1/2 before:h-[2rem] before:rounded-full before:w-[2rem] before:bg-transparent hover:before:bg-3a-white/10 before:transition-all before:duration-300"
       )}
     />
   </SwitchPrimitives.Root>
